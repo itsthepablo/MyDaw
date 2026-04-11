0e3bff6adf976650156afa71813a8964ddf4caf9
pablo frutos <pablo_the_gamer001@hotmail.com>
2026-04-10 23:07:48 -0300

    waveform zoom corregido

--- Source/Clips/Audio/UI/AudioClipRenderer.h
+++ Source/Clips/Audio/UI/AudioClipRenderer.h
@@ -2,7 +2,7 @@
 #include <JuceHeader.h>
 #include "../AudioClip.h"
 #include "../LookAndFeel/AudioClipLF.h"
-#include "../../../Data/GlobalData.h"
+#include "../../../GlobalData.h"
 
 /**
  * AudioClipRenderer: Se encarga de dibujar el AudioClip en la pantalla.
